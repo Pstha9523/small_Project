9d7b226afdcf607109faadcb5ad82b6209e8975c
Pratistha Shrestha <pratishresthatech@gmail.com>
2024-07-30 13:23:35 -0500

    Initial commit

---
+++ src/main/java/com/example/smallProject/SmallProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.smallProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smallProject/SmallProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smallProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
